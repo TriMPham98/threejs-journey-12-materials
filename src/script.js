8086454c0c526125c746c4014c956c4e6fbd1610
Tri <trimpham98@gmail.com>
2023-06-01 18:44:17 -0700

    Instantiate a sphere, plane, and torus and add to scene

--- src/script.js
+++ src/script.js
@@ -11,6 +11,23 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
+ * Objects
+ */
+const material = new THREE.MeshBasicMaterial();
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
+
+sphere.position.x = -1.5;
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+
+const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 16, 32), material);
+
+torus.position.x = 1.5;
+
+scene.add(sphere, plane, torus);
+
+/**
  * Sizes
  */
 const sizes = {
