218d01ede5dcad23b962a0c475c60383aab70ba7
Tri <trimpham98@gmail.com>
2023-06-01 21:54:56 -0700

    Add wireframe for material

--- src/script.js
+++ src/script.js
@@ -31,7 +31,8 @@ const scene = new THREE.Scene();
  * Objects
  */
 const material = new THREE.MeshBasicMaterial();
-material.map = doorColorTexture;
+// material.map = doorColorTexture;
+material.wireframe = true;
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 
